703ca0fedeafafe09aef37edd868f6857ae3d8af
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-06 18:41:20 +0430

    l

--- poblic/app.js
+++ poblic/app.js
@@ -1,3 +1,24 @@
+const body = document.getElementById("body");
+const mode = document.getElementById("mode");
+mode.addEventListener("click", () => {
+  console.log("hi");
+});
+// const dark = document.querySelector("button.bg-amber-100");
+// mode.addEventListener("click", () => {
+//   console.log("hi");
+//   if (body.classList.contains("bg-greenC")) {
+//     body.classList.remove("bg-greenC");
+//     body.classList.remove("text-amber-100");
+//     body.classList.add("text-greenC");
+//     body.classList.add("bg-amber-100");
+//   } else {
+//     body.classList.add("bg-greenC");
+//     body.classList.add("text-amber-100");
+//     body.classList.remove("text-greenC");
+//     body.classList.remove("bg-amber-100");
+//   }
+// });
+
 const observer = new IntersectionObserver((entres) => {
   entres.forEach(
     (entre) => {
